98e9772fb40e89d783c4d80e2fd8c219cd8ab730
0xBECEDA <sakharovasonya1@gmail.com>
2018-09-01 12:59:19 +0300

    output

--- 01_hello_SDL.cpp
+++ 01_hello_SDL.cpp
@@ -36,7 +36,6 @@ bool create() {
 int main( int argc, char* args[] )
 {
     //включаем SDL и создаем окно
-    // не знаю, что означает ! перед функциями, подозреваю, что так мы говорим "если функция такая-то не сработала",
     if( !init() ) {
         // печатаем сообщение об ошибке
         printf( "Failed to initialize!\n" );
@@ -53,7 +52,57 @@ int main( int argc, char* args[] )
             printf("Key is pressed.\n");
             break;
         default:
-            printf("Unhandled Event!\n");
+            //  printf("Unhandled Event type is: ( %d) !\n", test_event.type);
+            printf("SDL_FIRSTEVENT is %d\n", SDL_FIRSTEVENT);
+            printf("SDL_QUIT is %d\n", SDL_QUIT);
+            printf("SDL_APP_TERMINATING is %d\n", SDL_APP_TERMINATING);
+            printf("SDL_APP_LOWMEMORY is %d\n", SDL_APP_LOWMEMORY);
+            printf("SDL_APP_WILLENTERBACKGROUND is %d\n", SDL_APP_WILLENTERBACKGROUND);
+            printf("SDL_APP_DIDENTERBACKGROUND is %d\n", SDL_APP_DIDENTERBACKGROUND);
+            printf("SDL_APP_WILLENTERFOREGROUND is %d\n", SDL_APP_WILLENTERFOREGROUND);
+            printf("SDL_APP_DIDENTERFOREGROUND is %d\n", SDL_APP_DIDENTERFOREGROUND);
+            printf("SDL_WINDOWEVENT is %d\n", SDL_WINDOWEVENT);
+            printf("SDL_SYSWMEVENT is %d\n", SDL_SYSWMEVENT);
+            printf("SDL_KEYDOWN is %d\n", SDL_KEYDOWN);
+            printf("SDL_KEYUP is %d\n", SDL_KEYUP);
+            printf("SDL_TEXTEDITING is %d\n", SDL_TEXTEDITING);
+            printf("SDL_TEXTINPUT is %d\n", SDL_TEXTINPUT);
+            printf("SDL_KEYMAPCHANGED is %d\n", SDL_KEYMAPCHANGED);
+            printf("SDL_MOUSEMOTION is %d\n", SDL_MOUSEMOTION);
+            printf("SDL_MOUSEBUTTONDOWN is %d\n", SDL_MOUSEBUTTONDOWN);
+            printf("SDL_MOUSEBUTTONUP is %d\n", SDL_MOUSEBUTTONUP);
+            printf("SDL_MOUSEWHEEL is %d\n", SDL_MOUSEWHEEL);
+            printf("SDL_JOYAXISMOTION is %d\n", SDL_JOYAXISMOTION);
+            printf("SDL_JOYBALLMOTION is %d\n", SDL_JOYBALLMOTION);
+            printf("SDL_JOYHATMOTION is %d\n", SDL_JOYHATMOTION);
+            printf("SDL_JOYBUTTONDOWN is %d\n", SDL_JOYBUTTONDOWN);
+            printf("SDL_JOYBUTTONUP is %d\n", SDL_JOYBUTTONUP);
+            printf("SDL_JOYDEVICEADDED is %d\n", SDL_JOYDEVICEADDED);
+            printf("SDL_JOYDEVICEREMOVED is %d\n", SDL_JOYDEVICEREMOVED);
+            printf("SDL_CONTROLLERAXISMOTION is %d\n", SDL_CONTROLLERAXISMOTION);
+            printf("SDL_CONTROLLERBUTTONDOWN is %d\n", SDL_CONTROLLERBUTTONDOWN);
+            printf("SDL_CONTROLLERBUTTONUP is %d\n", SDL_CONTROLLERBUTTONUP);
+            printf("SDL_CONTROLLERDEVICEADDED is %d\n", SDL_CONTROLLERDEVICEADDED);
+            printf("SDL_CONTROLLERDEVICEREMOVED is %d\n", SDL_CONTROLLERDEVICEREMOVED);
+            printf("SDL_CONTROLLERDEVICEREMAPPED is %d\n", SDL_CONTROLLERDEVICEREMAPPED);
+            printf("SDL_FINGERDOWN is %d\n", SDL_FINGERDOWN);
+            printf("SDL_FINGERUP is %d\n", SDL_FINGERUP);
+            printf("SDL_FINGERMOTION is %d\n", SDL_FINGERMOTION);
+            printf("SDL_DOLLARGESTURE is %d\n", SDL_DOLLARGESTURE);
+            printf("SDL_DOLLARRECORD is %d\n", SDL_DOLLARRECORD);
+            printf("SDL_MULTIGESTURE is %d\n", SDL_MULTIGESTURE);
+            printf("SDL_CLIPBOARDUPDATE is %d\n", SDL_CLIPBOARDUPDATE);
+            printf("SDL_DROPFILE is %d\n", SDL_DROPFILE);
+            //   printf("SDL_DROPTEXT is %d\n", SDL_DROPTEXT);
+            // printf("SDL_DROPBEGIN is %d\n", SDL_DROPBEGIN);
+            //  printf("SDL_DROPCOMPLETE is %d\n", SDL_DROPCOMPLETE);
+            printf("SDL_AUDIODEVICEADDED is %d\n", SDL_AUDIODEVICEADDED);
+            printf("SDL_AUDIODEVICEREMOVED is %d\n", SDL_AUDIODEVICEREMOVED);
+            printf("SDL_RENDER_TARGETS_RESET is %d\n", SDL_RENDER_TARGETS_RESET);
+            printf("SDL_RENDER_DEVICE_RESET is %d\n", SDL_RENDER_DEVICE_RESET);
+            // printf("SDL_RegisterEvents() is %d\n", SDL_RegisterEvents());
+            printf("SDL_USEREVENT is %d\n", SDL_USEREVENT);
+            printf("SDL_LASTEVENT is %d\n", SDL_LASTEVENT);
             break;
         }
     }
