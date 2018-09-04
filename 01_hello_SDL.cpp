b43fdcf6193c5b4649d58c697a24f9df22face65
0xBECEDA <sakharovasonya1@gmail.com>
2018-09-04 13:46:47 +0300

    paint

--- 01_hello_SDL.cpp
+++ 01_hello_SDL.cpp
@@ -4,14 +4,18 @@
 // включить SDL и сделать окно
 bool init();
 bool create();
+bool surface_create ();
+void paint (SDL_Event* event);
 //окно, которое мы показываем
 SDL_Window* gWindow = NULL;
+SDL_Surface* surface = NULL;
 SDL_Event event;
 int SCREEN_WIDTH = 480;
 int SCREEN_HEIGHT = 520;
 int i;
 bool init()
 {
+
     //инициализация флага (?) что это такое?
     bool success = true;
     //инициализация SDL
@@ -25,10 +29,25 @@ bool init()
 }
 
 bool create() {
-  bool success = true;
+
+    bool success = true;
   gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
   return success;
 }
+
+
+bool surface_create ()
+{
+    bool  success = true;
+// создание поверхности окна
+    surface = SDL_GetWindowSurface( gWindow );
+
+    if ( surface == NULL ) {
+        printf ("Didn't create surface! SDL_Error: %s\n", SDL_GetError());
+    }
+    return success;
+}
+
 
 void PrintAllEvents()
 {
@@ -159,18 +178,141 @@ void PrintEvent(SDL_Event * event)
 }
 
 
+// находим пиксель
+Uint32 getpixel( SDL_Surface *surface, int x, int y) {
+// заблокируем поверхность
+    SDL_LockSurface(surface);
+    //выясняем формат пикселя
+    int bpp =  surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to retrieve */
+    Uint32 ppr = surface->pitch/bpp;
+    Uint8 *p = (Uint8 *)surface->pixels + (y * ppr + x )* bpp;
+
+    switch(bpp) {
+    case 1:
+        return *p;
+        break;
+
+    case 2:
+        return *(Uint16 *)p;
+        break;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            return p[0] << 16 | p[1] << 8 | p[2];
+        else
+            return p[0] | p[1] << 8 | p[2] << 16;
+        break;
+
+    case 4:
+        return *(Uint32 *)p;
+        break;
+
+    default:
+        return 0;       /* shouldn't happen, but avoids warnings */
+    }
+    // разблокируем поверхность
+    SDL_UnlockSurface(surface);
+}
+
+void DrawPixel(SDL_Surface *screen, int x, int y,
+               Uint8 R, Uint8 G, Uint8 B)
+{
+    Uint32 color = SDL_MapRGB(surface->format, R, G, B);
+
+    /* Here p is the address to the pixel we want to retrieve */
+
+    int bpp =  surface->format->BytesPerPixel;
+    Uint32 ppr = surface->pitch/bpp;
+
+
+    switch (bpp)
+    {
+    case 1: // Используем 8-bpp (бит на пиксель)
+    {
+
+        Uint8 *p = (Uint8 *)surface->pixels + (y * ppr + x )* bpp;
+
+        /* pixels – указатель на начало области данных,
+           описывающей состояние пикселей поверхности screen */
+        /* pitch – количество байт, занимаемых данными
+           о пикселях одной строки */      *p = color;
+    }
+    break;
+    case 2: // Возможно 15-bpp или 16-bpp
+    {
+        Uint16 *p = (Uint16 *)surface->pixels + (y * ppr + x );
+        /* Поскольку (Uint16 *)screen->pixels возвращает
+           указатель на целое число, занимающее в памяти 2 байта,
+           при вычислении смещения делим длину строки в байтах
+           на 2 */
+        *p = color;
+    }
+    break;
+    case 3: // Медленный режим 24-bpp, обычно не применяется
+    {
+        Uint8 *p = (Uint8 *)surface->pixels +
+            y*surface->pitch + x * 3;
+        if(SDL_BYTEORDER == SDL_LIL_ENDIAN)
+        { /* Учет возможного различия в порядке следования байт
+             в машинном слове различных процессорных архитектур */
+            p[0] = color;
+            p[1] = color >> 8;
+            p[2] = color >> 16;
+        } else {
+            p[2] = color;
+            p[1] = color >> 8;
+            p[0] = color >> 16;
+        }
+    }
+    break;
+    case 4: // Возможно 32-bpp...
+    {
+
+        // Раз у нас есть формула, учитывающая кол-во байтов на пиксель, зачем посредник в виде bufp?
+        Uint32 *p = (Uint32 *)surface->pixels + (y * ppr + x );
+        /* Поскольку (Uint32 *)screen->pixels возвращает
+           указатель на целое число, занимающее в памяти 4 байта,
+           при вычислении смещения делим длину строки в байтах
+           на 4 */
+        *p = color;
+    }
+    break;
+    }
+}
+
+void paint(& event)
+{
+    int x;
+    int y;
+    x = event.motion.x;
+    y = event.motion.y;
+
+    getpixel (surface, x, y);
+    DrawPixel(surface, x, y, 255, 255, 255);
+}
+
 int main( int argc, char* args[] )
 {
-    //включаем SDL и создаем окно
+    //включаем SDL
     if( !init() ) {
         // печатаем сообщение об ошибке, если инициализация не удалась
-        printf( "Failed to initialize!\n" );
+        printf( "Failed to initialize SDL!\n" );
     }
     // создаем окно
-    create();
-
+    if( !create() ) {
+        // печатаем сообщение об ошибке, если инициализация не удалась
+        printf( "Failed to initialize window!\n" );
+    }
+    //coздаем поверхность
+    if( !surface_create() ) {
+        // печатаем сообщение об ошибке, если инициализация не удалась
+        printf( "Failed to initialize surface!\n" );
+    }
     /* PrintAllEvents(); */
     // цикл, обрабатывающий события, пока не встретим событие "выход"
+    PrintAllEvents();
+
     while (256 != event.type) {
         //SDL_WaitEvent меньше нагружает комп
         SDL_WaitEvent(& event);
