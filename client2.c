49f0950746041e3f6cd3a74ca15c9f6387c87f26
0xBECEDA <sakharovasonya1@gmail.com>
2018-11-07 16:20:58 +0300

    loops

--- client2.c
+++ client2.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <fcntl.h>
 //char message[] = "Hello there!\n";
 
 // буфер, откуда сокет возьмет данные
@@ -30,6 +31,7 @@ void  main()
         exit(1);
     }
 
+    fcntl(sock, F_SETFL, O_NONBLOCK);
     //инициализируем struct sockaddr_in
 
     // семейство адресов AF_INET. Зачем писать еще раз, если уже
@@ -46,34 +48,52 @@ void  main()
     /* потому что на разных архитектурах и в разных ОС
        она может быть разной. Так что лучше пусть компилятор
        вычислит и подставит - погугли, что такое sizeof-макрос */
-    if (connect(sock, (struct sockaddr *)&addr,
-                sizeof(addr)) < 0) {
+    int r = -1;
+    while(r<0) {
+
+        r = connect(sock, (struct sockaddr *)&addr,
+                       sizeof(addr));
+        if (r < 0) {
+       //  break;
+       // }
         perror("connect");
-        exit(2);
-    }
-
-    while (1) {
-        // отправляем
-        /* вернее сначала читаем в буфер строку */
-        scanf("%s", buf);
-
-        // передаем дескриптор сокета, указатель на буфер с данными
-        // длинну буфера в байтах, флаги
-        // send возвращает кол-во отправленных байтов
-        send(sock, buf, sizeof(buf), 0);
-
-        int bytes_read =  recv(sock, buf, sizeof(buf), 0);
-
-        if (bytes_read > 0) {
-            // печатаем сообщение
-            printf("Server message: %s\n", buf);
-        } else {
-            // печатаем ошибку
-            perror("Client2 error");
-            exit(3);
+        //   exit(2);
         }
     }
 
+    while (1) {
+        printf("I am in loop");
+        // отправляем
+        /* вернее сначала читаем в буфер строку */
+        scanf("%s", buf);
+        int amount = 0;
+        while (1) {
+        // передаем дескриптор сокета, указатель на буфер с данными
+        // длинну буфера в байтах, флаги
+        // send возвращает кол-во отправленных байтов
+
+            amount = send(sock, buf, sizeof(buf), 0);
+            if (amount!=0) {
+                printf("amount %d\n", amount);
+                break;
+            }
+        }
+
+        while (1) {
+            int bytes_read =  recv(sock, buf, sizeof(buf), 0);
+
+            if (bytes_read > 0) {
+                // печатаем сообщение
+                printf("Server message: %s\n", buf);
+                fflush(stdout);
+            }
+            //else {
+                // печатаем ошибку
+                // perror("Client2 error");
+                // exit(3);
+            //}
+        }
+    }
     //закрываем сокет
     close(sock);
 }
