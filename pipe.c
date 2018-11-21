03f7a0c776c79fb75d9a95ff76d86b6304566287
0xBECEDA <sakharovasonya1@gmail.com>
2018-11-21 15:15:18 +0300

    comments

--- pipe.c
+++ pipe.c
@@ -10,33 +10,48 @@
 
 void toPipe (int inPipe[], char outstr[])
 {
+    /*len равна длине строки из массива outstr*/
     int len = strlen(outstr);
+    /*выводим длину*/
     printf(":: strlen(outstr) = %d\n", len);
     fflush(stdout);
+    /*записываем количество байтов len из outstr в inPipe[1] */
     int cnt = write(inPipe[1], outstr, len);
+    /*если количество байт -1, возвращаем сообщение об ошибке*/
     if (-1 == cnt) {
         perror("write to pipe");
         exit(-1);
     }
 }
 
+/*функция принимает массив интов, длинну и массив чаров*/
 void fromPipe(int outPipe[], int len, char retval[])
 {
+    /*объявляем массив на 1024 байта*/
     char buf[SIZE];
+    /*заполняем массив нулями*/
     memset(buf, 0, SIZE);
+    /*читаем в buf количество байт, равное len из канала,
+      который находится в outPipe[0]*/
     int cnt = read(outPipe[0], buf, len);
+    /*если количество байт -1, возвращаем ошибку*/
     if (-1 == cnt) {
         perror("read from pipe");
         exit(-1);
     }
+    /*выводим количество прочитанныз байт из буфера (?)*/
     printf(":: %d [child out]\n%s\n", cnt, buf);
     fflush(stdout);
+    /*если retval не равно нулю и при этом длина больше 1024 байт,
+      то печатаем сообщение об ошибке*/
     if (NULL != retval) {
         if (SIZE <= len) {
             printf("out of buf");
             fflush(stdout);
             exit(-1);
         }
+        /*в противном случае копируем в retval символы из buf,
+          равные количеству len*/
         strncpy(retval, buf, len);
     }
 }
@@ -81,19 +96,26 @@ int main(int argc, char *argv[])
         /* child stdin! ->  pipe in  */
         /*создаем дубликат файлового дескриптора*/
         dup2(inPipe[0], 0);
+        /*закрываем увнвл с данными*/
         close(inPipe[1]);
         /* child stdout ->  pipe out */
+        /*аналогично*/
         close(outPipe[0]);
+        /*дублируем дескриптор*/
         dup2(outPipe[1], 1);
         /* run program */
+        /* запускаем новую программу вместо программы,
+        которая ее вызвала. Старой программе недоступны сегменты
+        памяти, перезаписанные новой программой*/
         execvp("./interact", argv);
         return 0;
     }
 
     /* parent process */
+    /*не вижу инициализацию pid, соответственно не понимаю, что можно       там вывести кроме мусора или 0*/
     printf(":: pid = %d\n", pid);
     fflush(stdout);
-
+    /*объявляем массив на 1024 байата*/
     char result[SIZE];
 
     fromPipe(outPipe, 30, NULL);
@@ -102,11 +124,13 @@ int main(int argc, char *argv[])
     toPipe(inPipe, "7\n");
     fromPipe(outPipe, 30, result);
     toPipe(inPipe, "y\n");
-
+    /*не понимаю, что это.
+      Нагуглила, что strcspn ищет первое вхождение в строку. */
     int pos = strcspn(result, "\n");
+    /*что это?*/
     *(result+pos) = 0;
     fromPipe(outPipe, 30, NULL);
-
+    /*вывод результата*/
     printf("Result is [%s]\n", result);
     fflush(stdout);
 
