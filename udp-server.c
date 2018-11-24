3e0042c6b93ee7b56f4dcb6923f7e4cc973ad526
0xBECEDA <sakharovasonya1@gmail.com>
2018-11-24 13:24:46 +0300

    look

--- udp-server.c
+++ udp-server.c
@@ -1,21 +1,48 @@
 // Server side implementation of UDP client-server model
+//#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <errno.h>
+#include <linux/unistd.h>
 
 #define PORT     8080
 #define MAXLINE 1024
 
+struct pixel
+{
+    bool alive = false;
+    int c;
+    int d;
+} pixels[100];
+
+
+struct box
+{
+    int c;
+    int d;
+} pixels_box[100];
+
+
+struct connection
+{
+    int thread;
+    int connection;
+    int p;
+    int point;
+} clients[1];
+
 // Driver code
 void  main() {
     int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from server";
+
     struct sockaddr_in servaddr, cliaddr;
 
     // Создаем сокет. Должны в случае успеха получить его дескриптор
@@ -43,6 +70,11 @@ void  main() {
         exit(EXIT_FAILURE);
     }
 
+    void* pointer = NULL;
+
+    pthread_t udp_thread;
+
+    pthread_create(&udp_thread, NULL, udp_socket, pointer);
     int len, n;
     len = sizeof(cliaddr);
     //получаем пакет от клиента
@@ -53,14 +85,14 @@ void  main() {
     // флаги, указатель на структуру с данными клиента, ссылку на
     // длинну структуры клиента
 
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-                 MSG_WAITALL, ( struct sockaddr *) &cliaddr,
-                 &len);
+    //  n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+    //             MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+    //             &len);
     // зачем приравнивать буфер, равный теперь длинне сообщения из
     // recvfrom, к знаку окончания симфольной строки?
     //buffer[n] = '\0';
 
-    printf("Client : %s\n", buffer);
+    // printf("Client : %s\n", buffer);
 
 
     //отправляем пакет
@@ -69,9 +101,9 @@ void  main() {
     // буфер с данными, длинну данных, флаги, указатель на структуру,
     // содержащую данные клиента, размер структуры
 
-    sendto(sockfd, (const char *)hello, strlen(hello),
-           MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
-           len);
+// sendto(sockfd, (const char *)hello, strlen(hello),
+    //     MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+    //     len);
     printf("Hello message sent.\n");
 
 }
